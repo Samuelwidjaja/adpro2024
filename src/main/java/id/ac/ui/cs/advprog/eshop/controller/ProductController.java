ab5f2469d2730d01bebc7db990d4da412cd7ee83
Samuelwidjaja <james.samuel.widjaja@gmail.com>
2024-02-02 20:21:50 +0700

    fix controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -31,7 +31,7 @@ public class ProductController {
     @GetMapping("/list")
     public String productListPage(Model model){
         List<Product> allProducts = service.findAll();
-        model.addAttribute("products, allProducts");
+        model.addAttribute("products", allProducts);
         return "productList";
     }
 }
