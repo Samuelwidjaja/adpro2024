c9ee1849c8c3ea9fe133dc7bb363d1dd2e27722a
Samuelwidjaja <james.samuel.widjaja@gmail.com>
2024-02-24 17:31:38 +0700

    fix before-solid

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -1,7 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
-import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
+
+import id.ac.ui.cs.advprog.eshop.controller.model.Product;
 
 import java.util.ArrayList;
 import java.util.Iterator;
--- src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
@@ -1,6 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
-import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.controller.model.Product;
 import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
