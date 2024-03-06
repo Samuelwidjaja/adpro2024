fc113d168b5039fb3969889488606b2105982154
Samuelwidjaja <james.samuel.widjaja@gmail.com>
2024-03-06 20:29:10 +0700

    [GREEN] Implement PaymentServiceImpl class

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -6,6 +6,9 @@ import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
 
 
 import org.springframework.stereotype.Service;
+
+import enums.OrderStatus;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Map;
@@ -18,21 +21,34 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment addPayment(String paymentId, Order order, String method, Map<String, String> paymentData) {
+        if (paymentRepository.findById(paymentId) == null){
+            Payment payment = new Payment(paymentId, method, order, paymentData);
+            paymentRepository.save(payment);
+            return payment;
+        }
         return null;
     }
 
     @Override
     public Payment setStatus(Payment payment, String status) {
+        Order currentOrder = payment.getOrder();
+        if (status.equals("SUCCESS")) {
+            currentOrder.setStatus(OrderStatus.SUCCESS.getValue());
+        } else if (status.equals("REJECTED")) {
+            currentOrder.setStatus(OrderStatus.FAILED.getValue());
+        } else {
+            throw new IllegalArgumentException("Invalid status payment");
+        }
         return payment;
     }
 
     @Override
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
 
     @Override
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.getAllPayment();
     }
 }
